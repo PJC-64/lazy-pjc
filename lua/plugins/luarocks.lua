67b44c918d828d9c86c0fd3a7793f8bf088b9599
Paul Cutmore <paul.cutmore@icloud.com>
2024-05-20 20:35:57 +0100

    fix(app): Commented out luarocks and neorg

--- lua/plugins/luarocks.lua
+++ lua/plugins/luarocks.lua
@@ -1,12 +1,14 @@
 return {
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 20000,
-    opts = {
-      rocks = {
-        "fzy",
-        "pathlib.nvim ~> 1.0",
-      },
-    },
-  },
+  -- NOTE: Currently commented out to disable Rocks.
+  -- TODO: Delete this file once I'm sure I don't need or want to use luarocks.
+  -- {
+  --   "vhyrro/luarocks.nvim",
+  --   priority = 20000,
+  --   opts = {
+  --     rocks = {
+  --       "fzy",
+  --       "pathlib.nvim ~> 1.0",
+  --     },
+  --   },
+  -- },
 }
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,9 +1,11 @@
 return {
-  {
-    "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
-    lazy = false,
-    version = "*",
-    config = true,
-  },
+  -- NOTE: Currently commented out to disable Neorg.
+  -- TODO: Delete this file once I'm sure I don't need or want to use neorg.
+  --   {
+  --     "nvim-neorg/neorg",
+  --     dependencies = { "luarocks.nvim" },
+  --     lazy = false,
+  --     version = "*",
+  --     config = true,
+  --   },
 }
